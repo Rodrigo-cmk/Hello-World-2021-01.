3afe859877c03d144d68cf89b0d3770fe5ebe6ae
Rodrigo Alves <rodrigorac.2003@gmail.com>
2021-02-24 14:53:54 -0300

    Primeira Versão do Pograma

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_world_2021_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
